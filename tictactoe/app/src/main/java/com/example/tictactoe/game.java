8767d14e3aa337437826bf75290ef22107eee10d
Buzzed-Light <130939723+Buzzed-Light@users.noreply.github.com>
2023-04-25 20:46:35 -0400

    This is my change

--- tictactoe/app/src/main/java/com/example/tictactoe/game.java
+++ tictactoe/app/src/main/java/com/example/tictactoe/game.java
@@ -1,5 +1,7 @@
 package com.example.tictactoe;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -194,6 +196,7 @@ public class game extends Fragment {
             }
             if (currentGameInstance.isGameOver()) {
                //TODO Buzzed: Pop up dialog box
+
             } else {
                 currentGameInstance.players.changeCurrentPlayer();
             }
@@ -203,6 +206,37 @@ public class game extends Fragment {
 
     public void RowsColl(int row, int col) {
         Log.d("TAG", "Button clicked at row " + row + ", column " + col);
+    }
+
+    public void createPopUp() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(fragment_game.this);
+
+        // Set the message show for the Alert time
+        builder.setMessage("Do you want to exit ?");
+
+        // Set Alert Title
+        builder.setTitle("Alert !");
+
+        // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+        builder.setCancelable(false);
+
+        // Set the positive button with yes name Lambda OnClickListener method is use of DialogInterface interface.
+        builder.setPositiveButton("Yes", (DialogInterface.OnClickListener) (dialog, which) -> {
+            // When the user click yes button then app will close
+            finish();
+        });
+
+        // Set the Negative button with No name Lambda OnClickListener method is use of DialogInterface interface.
+        builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+            // If user click no then dialog box is canceled.
+            dialog.cancel();
+        });
+
+        // Create the Alert dialog
+        AlertDialog alertDialog = builder.create();
+        // Show the Alert Dialog box
+        alertDialog.show();
+
     }
 
 
